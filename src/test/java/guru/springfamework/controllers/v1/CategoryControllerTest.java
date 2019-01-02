bf9a08aec92b665eb054ca1d13cd04293883e539
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2019-01-02 17:20:09 +0000

    create rest api to return categories all and by name. Note when using injectMocks in test then do not need to to create constructor Also to product different forms of return type then use produce in annotation getMapping

---
+++ src/test/java/guru/springfamework/controllers/v1/CategoryControllerTest.java
@@ -0,0 +1,74 @@
+package guru.springfamework.controllers.v1;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import guru.springfamework.services.CategoryService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public class CategoryControllerTest {
+
+    @Mock
+    private CategoryService categoryService;
+
+    @InjectMocks
+    private CategoryController categoryController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(categoryController).build();
+    }
+
+    @Test
+    public void returnAllCategories() throws Exception {
+
+        CategoryDTO category1 = new CategoryDTO();
+        category1.setId(1L);
+        category1.setName("category1");
+
+        CategoryDTO category2 = new CategoryDTO();
+        category1.setId(2L);
+        category1.setName("category2");
+
+        when(categoryService.getAllCategories()).thenReturn(Arrays.asList(category1, category2));
+
+        mockMvc
+                .perform(get("/api/v1/categories/")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.categories", hasSize(2)));
+    }
+
+    @Test
+    public void returnCategoryByName() throws Exception {
+        CategoryDTO category1 = new CategoryDTO();
+        category1.setId(1L);
+        category1.setName("category1");
+
+        when(categoryService.getCategoryByName("category1")).thenReturn(category1);
+
+        mockMvc
+                .perform(get("/api/v1/categories/category1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", equalTo("category1")));
+    }
+}
