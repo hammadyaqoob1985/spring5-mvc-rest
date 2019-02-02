58cfedb8d88769fc9781df3089c14e71f5ef7d5c
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2019-02-02 21:39:56 +0000

    added swagger documentation for vendor

--- src/main/java/guru/springfamework/api/v1/model/VendorDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/VendorDTO.java
@@ -1,6 +1,7 @@
 package guru.springfamework.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -8,7 +9,9 @@ import lombok.Data;
  */
 @Data
 public class VendorDTO {
+    @ApiModelProperty(value = "name", required = true)
     private String name;
+    @ApiModelProperty(value = "vendor url", readOnly = true)
     @JsonProperty("vendor_url")
     private String vendorUrl;
 }
