c8c2b76f0d2bc084bd199637828196222a891455
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2019-02-02 12:27:10 +0000

    added meta data

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -19,6 +23,24 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Hammad Yaqoob",
+                "http://www.desifiedrecipes.co.uk", "hammad.yaqoob1985@yahoo.co.uk");
+
+        return new ApiInfo("Hammad Yaqoob",
+                "Test vendor api",
+                "1.0",
+                "blah",
+                contact,
+                "Apache license version 2.0",
+                "blah",
+                new ArrayList<>());
+
+
     }
 }
